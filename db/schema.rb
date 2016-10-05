8bca9f1f43203417f0ed640b2433bdda2f73e068
Bruno Kinoshita <brunokino@gmail.com>
2016-10-05 01:41:56 +0000

    first

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160327152119) do
+ActiveRecord::Schema.define(version: 20160331000049) do
 
   create_table "bands", force: :cascade do |t|
     t.string   "name"
@@ -20,5 +20,15 @@ ActiveRecord::Schema.define(version: 20160327152119) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
+
+  create_table "musics", force: :cascade do |t|
+    t.string   "title"
+    t.string   "name_author"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "band_id"
+  end
+
+  add_index "musics", ["band_id"], name: "index_musics_on_band_id"
 
 end
